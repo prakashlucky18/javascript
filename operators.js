80209398c818125b3c9dec71048bf9dc8859d964
prakash <prakashavala411@gmail.com>
2023-12-12 12:07:23 +0530

    first commit

---
+++ operators.js
@@ -0,0 +1,44 @@
+// operators in javacript
+let x=5;
+let y=10;
+document.write(x+y);
+document.write("<br>");
+document.write(x-y);
+document.write("<br>");
+document.write(x*y);
+document.write("<br>");
+document.write(x/y);
+document.write("<br>");
+document.write(x%y);
+document.write("<br>");
+// comparison operators
+document.write(x==y);
+document.write("<br>");
+document.write(x>y);
+document.write("<br>");
+document.write(x<y);
+document.write("<br>");
+document.write(x>=y);
+document.write("<br>");
+document.write(x<=y);
+document.write("<br>");
+document.write(x!=y);
+document.write("<br>");
+// Logical operators
+let a=true;
+let b=false;
+document.write(a&&b);
+document.write("<br>");
+document.write(a||b);
+document.write("<br>");
+document.write(!a);
+document.write("<br>");
+// unary operators
+let c=5;
+document.write(-c);
+document.write("<br>");
+document.write(++c);
+document.write("<br>");
+document.write(--c);
+
+
